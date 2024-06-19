6480474a21c7d247f56781c6e40a9c3717747e30
Ilham Santoso <ilhamsantoso007@gmail.com>
2024-06-19 12:27:59 +0700

    Read (Real-Time) dan Delete Data dengan Firebase

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -4,8 +4,13 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import Chart from "../../components/chart/Chart";
 import List from "../../components/datatable/Datatable";
+import Datatable from '../../components/datatable/Datatable';
+import { useLocation } from 'react-router-dom';
 
-const Single = () => {
+const Single = ({columns}) => {
+  const location = useLocation;
+  const id = location.pathname.split('/')[2];
+
   return (
     <div className="single">
       <Sidebar />
@@ -24,8 +29,8 @@ const Single = () => {
     <div className="details">
       <h1 className="itemTitle">Jane Doe</h1>
       <div className="detailItem">
-        <span className="itemKey">Email:</span>
-        <span className="itemValue">janedoe@gmail.com</span>
+        <span className="itemKey">ID:</span>
+        <span className="itemValue">{id}</span>
       </div>
       <div className="detailItem">
         <span className="itemKey">Phone:</span>
@@ -50,6 +55,7 @@ const Single = () => {
         </div>
         <div className="bottom">
   <h1 className="title">Last Transactions</h1>
+  <Datatable columns={columns} />
   <List />
 </div>
       </div>
